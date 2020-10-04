07e8a799a3e27f5a163207f3bfc167acba501670
Alexander Nikolayev <alexnikolayev.dev@gmail.com>
2020-10-04 10:44:36 +0300

    refactor: add more concise condition for empty array

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 exports.min = function min(array) {
-    return !Array.isArray(array) || array.length === 0 ? 0 : Math.min(...array);
+    return !Array.isArray(array) || array.length < 1 ? 0 : Math.min(...array);
 };
 
 exports.max = function max(array) {
-    return !Array.isArray(array) || array.length === 0 ? 0 : Math.max(...array);
+    return !Array.isArray(array) || array.length < 1 ? 0 : Math.max(...array);
 };
 
 exports.avg = function avg(array) {
-    return !Array.isArray(array) || array.length === 0
+    return !Array.isArray(array) || array.length < 1
         ? 0
         : array.reduce((a, b) => a + b) / array.length;
 };
